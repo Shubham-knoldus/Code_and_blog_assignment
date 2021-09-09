2aab4cbc2c554294403595ac4669c69dba7865e0
Shubham-knoldus <shubham.shrivastava@knoldus.com>
2021-09-09 23:17:12 +0530

    Code and Blog Assignment

---
+++ src/lsp/LSP.java
@@ -0,0 +1,16 @@
+package lsp;
+
+import lsp.Father;
+
+public class LSP {
+    public static void main(String[] args) {
+        Father f = new Son();
+        Son s = new Son();
+        f.reduction("reduction");
+        s.reduction("reduction");
+
+        f.enlarge("enlarge");
+        s.enlarge("enlarge");
+    }
+
+}
---
+++ src/lsp/Son.java
@@ -0,0 +1,17 @@
+package lsp;
+
+import lsp.Father;
+
+class Son extends Father {
+
+
+    public void reduction(String str) {
+        System.out.println("son : " + str);
+    }
+
+
+    public void enlarge(CharSequence str){
+        System.out.println("son : " + str);
+    }
+
+}
